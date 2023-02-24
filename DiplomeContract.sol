3fd2cbbba2e25ca1c143ecf1000fc161e0076714
Marius GAUDIN <g-marius@hotmail.fr>
2023-02-24 09:12:59 +0100

    Add diplome

--- DiplomeContract.sol
+++ DiplomeContract.sol
@@ -1,94 +1,132 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+import "./Token.sol";
 
 contract DiplomeContract {
-  address private owner;
+    address private owner;
+    address private token;
 
-  struct Etablisement {
-      string Nom;
-      string Type;
-      string Pays;
-      string Adresse;
-      string SiteWeb;
-      uint256 AgentId;
-  }
+    struct Etablisement {
+        string Nom;
+        string Type;
+        string Pays;
+        string Adresse;
+        string SiteWeb;
+        uint256 AgentId;
+    }
 
-  struct Etudiant {
-    uint256 Id;
-    string Nom;
-    string Prenom;
-    uint256 DateDeNaissance;
-    string Sexe;
-    string Nationalite;
-    string StatutCivil;
-    string Adresse;
-    string Courriel;
-    string Telephone;
-    string Section;
-    string SujetPfe;
-    string EntrepriseStagePfe;
-    string NomEtPrenomMaitreDuStage;
-    uint256 DateDebutStage;
-    uint256 DateFinStage;
-    string Evaluation;
-  }
+    struct Etudiant {
+        string Nom;
+        string Prenom;
+        uint256 IdEtablisement;
+        uint256 IdEntreprise;
+        uint256 DateDeNaissance;
+        string Sexe;
+        string Nationalite;
+        string StatutCivil;
+        string Adresse;
+        string Courriel;
+        string Telephone;
+        string Section;
+        string SujetPfe;
+        string EntrepriseStagePfe;
+        string NomEtPrenomMaitreDuStage;
+        uint256 DateDebutStage;
+        uint256 DateFinStage;
+        string Evaluation;
+    }
 
-  struct Diplome {
-      uint256 EtudiantId;
-      string NomEtablissement;
-      uint256 EtablissementId;
-      string Pays;
-      string Type;
-      string Specialite;
-      string Mention;
-      uint256 DateObtention;
-  }
+    struct Diplome {
+        uint256 EtudiantId;
+        string NomEtablissement;
+        uint256 EtablissementId;
+        string Pays;
+        string Type;
+        string Specialite;
+        string Mention;
+        uint256 DateObtention;
+    }
 
-  struct Entreprise {
-      string Nom;
-      string Secteur;
-      uint256 DateCreation;
-      string Taille;
-      string Pays;
-      string Adresse;
-      string Courriel;
-      string Telephone;
-      string SiteWeb;
-  }
+    struct Entreprise {
+        string Nom;
+        string Secteur;
+        uint256 DateCreation;
+        string Taille;
+        string Pays;
+        string Adresse;
+        string Courriel;
+        string Telephone;
+        string SiteWeb;
+    }
 
-  mapping(address => uint256) AddressAgents;
-  mapping(address => uint256) public AddressEntreprises;
-  mapping(uint256 => Etablisement) public Etablisements;
-  mapping(uint256 => Diplome) public Diplomes;
-  mapping(uint256 => Entreprise) public Entreprises;
-  mapping(uint256 => Etudiant) public Etudiants;
+    mapping(address => uint256) AddressAgents;
+    mapping(uint256 => Etablisement) public Etablisements;
 
-  uint256 public NbAgents;
-  uint256 public NbDiplomes;
-  uint256 public NbEntreprises;
-  uint256 public NbEtudiants;
+    mapping(uint256 => Etudiant) Etudiants;
 
-  constructor() {
-      owner = msg.sender;
-      NbAgents = 0;
-      NbEntreprises = 0;
-      NbDiplomes = 0;
-  }
+    mapping(uint256 => Diplome) public Diplomes;
+
+    mapping(uint256 => Entreprise) public Entreprises;
+    mapping(address => uint256) public AddressEntreprises;
+
+
+    uint256 public NbAgents;
+    uint256 public NbDiplomes;
+    uint256 public NbEntreprises;
+    uint256 public NbEtudiants;
+
+    constructor(address tokenaddress) {
+        token = tokenaddress;
+        owner = msg.sender;
+        NbAgents = 0;
+        NbEntreprises = 0;
+        NbDiplomes = 0;
+    }
     
-  // Un agent d’un établissement d’enseignement supérieur peut créer un compte pour
-  // son établissement qui va servir à enregistrer les jeunes diplômés et leurs diplômes.
-  function create_etablissement_account(string memory nom, string memory pays, string memory typeEtablissement, string memory adresse, string memory siteWeb) public {
-      Etablisement memory e;
-      e.Nom = nom;
-      e.Pays = pays;
-      e.Adresse = adresse;
-      e.SiteWeb = siteWeb;
-      e.Type = typeEtablissement;
-      NbAgents += 1;
-      e.AgentId = NbAgents;
-      AddressAgents[msg.sender] = e.AgentId;
-      Etablisements[e.AgentId] = e;
-  }
+    // Un agent d’un établissement d’enseignement supérieur peut créer un compte pour
+    // son établissement qui va servir à enregistrer les jeunes diplômés et leurs diplômes.
+    function create_etablissement_account(string memory nom, string memory pays, string memory typeEtablissement, string memory adresse, string memory siteWeb) public {
+        Etablisement memory e;
+        e.Nom = nom;
+        e.Pays = pays;
+        e.Adresse = adresse;
+        e.SiteWeb = siteWeb;
+        e.Type = typeEtablissement;
+        NbAgents += 1;
+        e.AgentId = NbAgents;
+        AddressAgents[msg.sender] = e.AgentId;
+        Etablisements[e.AgentId] = e;
+    }
+
+    function evaluate_student(        
+        uint256 etudiantid,
+        string memory sujetPfe,
+        string memory entrepriseStagePfe,
+        string memory nomEtPrenomMaitreDuStage,
+        uint256 dateDebutStage,
+        uint256 dateFinStage,
+        string memory evaluation
+    ) public {
+
+        uint256 id = AddressEntreprises[msg.sender];
+        require(Etudiants[etudiantid].IdEntreprise == id, "Etudient non rattache a l'entreprise");
+        Etudiants[etudiantid].SujetPfe = sujetPfe;
+        Etudiants[etudiantid].EntrepriseStagePfe = entrepriseStagePfe;
+        Etudiants[etudiantid].NomEtPrenomMaitreDuStage = nomEtPrenomMaitreDuStage;
+        Etudiants[etudiantid].DateDebutStage = dateDebutStage;
+        Etudiants[etudiantid].DateFinStage = dateFinStage;
+        Etudiants[etudiantid].Evaluation = evaluation;
+
+        require(
+            Token(token).allowance(owner, address(this)) >= 15,
+            "Erreur transaction non valide"
+        );
+
+        require(
+            Token(token).transferFrom(owner, msg.sender, 15),
+            "Erreur transaction"
+        );
+    }
 
     /**Un agent d’un établissement d’enseignement supérieur peut créer et sauvegarder
     un profil pour un étudiant lorsque ce dernier commence son stage de fin d’étude.
@@ -110,16 +148,6 @@ contract DiplomeContract {
 
         NbEtudiants += 1;
         Etudiants[NbEtudiants] = e;
-    }
-        
-    function update_student(Diplome memory d)private{
-        NbDiplomes += 1;
-        Diplomes[NbDiplomes] = d;
-    }
-
-    function create_diplome(Diplome memory d) private {
-        NbDiplomes += 1;
-        Diplomes[NbDiplomes] = d;
     }
 
     // Un agent de recrutement peut créer un compte pour son entreprise.
@@ -150,30 +178,21 @@ contract DiplomeContract {
             AddressEntreprises[msg.sender] = NbEntreprises;
     }
 
-function add_diplome(
-    uint256 id_titulaire, 
-    string memory nom_etablisement, 
-    uint256 id_etablisement, 
-    string memory pays, 
-    string memory typeEtablissement, 
-    string memory specialite, 
-    string memory mention, 
-    uint256 date_obtention) public {
-        require(AddressAgents[msg.sender] == id_etablisement, "not authorized to create diploma for this establishment");
-
-        uint256 id = Etudiants[id_titulaire].Id;
-        require(id != 0, "student does not exist");
-        
-        Diplome memory d;
-        d.EtudiantId = id_titulaire;
-        d.NomEtablissement = nom_etablisement;
-        d.EtablissementId = id_etablisement;
-        d.Pays = pays;
-        d.Type = typeEtablissement;
-        d.Specialite = specialite;
-        d.Mention = mention;
-        d.DateObtention = date_obtention;
-        create_diplome(d);
+    function add_diplome (
+        uint256 idTitulaire, 
+        string memory nomEtablisement, 
+        uint256 idEtablisement, 
+        string memory mention, 
+        uint256 dateObtention) public {
+            require(AddressAgents[msg.sender] == idEtablisement, "Pas autorise a creer un diplome pour l'etablissement");
+            Etudiant memory etud = Etudiants[idTitulaire];
+            require(etud.IdEtablisement == idEtablisement, "L'etudiant ne fait pas partie de l'etablissement");
+            Diplome memory d;
+            d.EtudiantId = idTitulaire;
+            d.NomEtablissement = nomEtablisement;
+            d.EtablissementId = idEtablisement;
+            d.Mention = mention;
+            d.DateObtention = dateObtention;
     }
 
 }
